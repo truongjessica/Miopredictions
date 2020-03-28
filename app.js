4435a7d5c6c29d29802896e318bbbd57a3ee5dd9
Lyd <lydiama8@gmail.com>
2020-03-28 15:40:22 -0700

    travel time paths and db stuff

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var routes = require('./routes/routes.js');
 var app = express();
 
 // view engine setup
@@ -18,9 +15,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +31,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+app.use('/', routes.get_homepage);
+app.use('/users', routes.get_userslisting);
+app.post('/posttraveltime', routes.post_predictedtime);
 
 module.exports = app;
--- database/database.js
+++ database/database.js
@@ -20,3 +20,43 @@ var firebaseConfig = {
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
+
+//-------------------------------------------------//
+
+var myDB_getTravelTime = function(startCity, endCity, startInt, endInt, time, callback) {
+    console.log('Getting average time of' + startInt + ' to ' + endInt + ' at '+ time);
+    var source = db.collection(startCity);
+    var results = null;
+    //check var names w Jason; can i compare time stamps like that? 
+
+    source.where("startLocation", "==", startInt).where("destination", "==", endInt)
+        .get()
+        .then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                var document = doc.data();
+                var startofPeriod = doc.get("startTime");   //start of 15 minute time period    
+                var endOfPeriod = doc.get("endTime");   //end of 15 minute time period
+
+                if (startTime <= time && endTime >= time) {     //if the time the person is travelling is within this binned interval....
+                    console.log('success');
+                    console.log(doc.id, " => ", document);
+                    results = doc.get("traveltime");
+                }
+            });
+        })
+        .catch(function(error) {
+            console.log("Error getting documents: ", error);
+        });    
+
+   callback(results, null);
+}
+
+//var source = db.collection("testcity");
+//myDB_getTravelTime("CityA", "CityB", "Dorchester Road and Huron Church Road", "Grand Blvd W and Jeffries Fwy SSD", new firebase.firestore.Timestamp.now());
+
+
+var database = {
+    getTravelTime: myDB_getTravelTime
+}
+
+module.exports = database;
