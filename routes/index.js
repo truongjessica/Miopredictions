c9973a98195685eec9ace4c6986f4a6bee84d262
orr1nuycruz <orr1n@hotmail.com>
2020-03-28 19:04:45 -0400

    add image and re ordered files

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/results', function(req, res, next) {
+  res.render('results', { title: 'Hello' });
+});
+
 module.exports = router;
