8235c556b4c3630d181e8a71262a73baee926857
orr1nuycruz <orr1n@hotmail.com>
2020-03-28 21:34:07 -0400

    added animation

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('selectTown', { title: 'Select Town' });
 });
+
+
+router.get('/intersection', function(req, res, next) {
+  res.render('intersection', { title: 'Intersection' });
+});
+
+
 
 router.get('/results', function(req, res, next) {
   res.render('results', { title: 'Hello' });
