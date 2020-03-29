9e90f3a64b8c09486e4792c2050bd69eecbb86a7
Ifavhoney <nuthsaid@outlook.com>
2020-03-28 22:20:22 -0400

    Did all remaining api routing

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-var createError = require('http-errors');
+var createError = require('http-errors')
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var routes = require('./routes/routes.js');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var app = express();
 
 // view engine setup
@@ -16,13 +19,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -33,8 +39,10 @@ app.use(function(err, req, res, next) {
 });
 
 
-app.use('/', routes.get_homepage);
+/*
+app.use('/users', routes.get_homepage);
+
 app.use('/users', routes.get_userslisting);
 app.post('/posttraveltime', routes.post_predictedtime);
-
+*/
 module.exports = app;
